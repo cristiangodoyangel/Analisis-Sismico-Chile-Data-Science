a38c8f49c95d66c7144d0e86dab3060fce048150
cristiangodoyangel <creacciones@gmail.com>
2025-06-23 11:53:48 -0400

    se convierte la fecha a año y mes, separados por columna

--- Analisis_Sismico_Chile.py
+++ Analisis_Sismico_Chile.py
@@ -16,13 +16,38 @@ print("======================= Carga de datos correcta =======================")
 print("\n")
 print(df_1.head())
 
+# revisamos la informacion del dataset, lo primero siempre es conocer los datos que tenemos.
 print("\n")
 print("======================= Información del Dataset Sismico =======================")
 print("\n")
 print(df_1.info())  
 
+# revisamos la estadistica descriptiva del dataset.
 print("\n")
 print("======================= Estadística Descriptiva del Dataset Sismico =======================")
 print("\n")
 print(df_1.describe().round(1).T)
 print("\n")
+
+# con la estadistica descriptiva podemos ver que el dataset tiene 4018 filas y 5 columnas.
+# con la información del dataser podemos ver que la fecha no está en el formato correcto, 
+# está en string, si necesitamos generar estadisticas sobre la fecha, debemos convertirlo a datetime.
+
+# convertimos la fecha a datetime.
+# Convertir a zona horaria de Chile
+df_1['Fecha'] = pd.to_datetime(df_1['Date(UTC)'])
+
+# todos los datos están en UTC, por lo que debemos convertirlos a Chile
+df_1['Fecha_Chile'] = df_1['Fecha'].dt.tz_localize('UTC').dt.tz_convert('America/Santiago')
+
+# se crean por separado, año y mes
+df_1['Año'] = df_1['Fecha_Chile'].dt.year
+df_1['Mes'] = df_1['Fecha_Chile'].dt.month
+
+# verificamos la correcta conversión de la fecha.
+print("\n")
+print("======================= Verificación de Año y Mes =======================")
+print(df_1[['Fecha_Chile', 'Año', 'Mes']].head())
+
+#
+
